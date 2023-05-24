5dcdfb01a8cde146a3bf5c88301fbb490dfcddaa
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-05-24 18:08:59 +0000

    exercise completed

--- src/app.js
+++ src/app.js
@@ -16,15 +16,21 @@ let generateExcuse = () => {
     "massive",
     "enormous",
     "ginormous",
-    "crazy"
+    "crazy",
+    "fucking"
   ];
   const noun = ["jogger", "racoon", "dog", "driver", "clown", "pinecone"];
+  const extension = [".com", ".net", ".us", ".io", ".es"];
 
   const pronounIndx = Math.floor(Math.random() * pronoun.length);
-  const adjectiveIndx = Math.floor(Math.random() * noun.length);
+  const adjectiveIndx = Math.floor(Math.random() * adjective.length);
   const nounIndx = Math.floor(Math.random() * noun.length);
+  const extensionIndx = Math.floor(Math.random() * extension.length);
 
   return (
-    pronoun[pronounIndx] + " " + adjective[adjectiveIndx] + " " + noun[nounIndx]
+    pronoun[pronounIndx] +
+    adjective[adjectiveIndx] +
+    noun[nounIndx] +
+    extension[extensionIndx]
   );
 };
